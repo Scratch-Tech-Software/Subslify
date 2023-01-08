01d14d1d2c7204823f9bf9f203045362e0d0bfef
Chris K <Chris-SourceCode@outlook.com>
2023-01-08 03:52:57 +0100

    feat: Add App.jsx and implement React.Router

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Register, Landing, Error } from './pages';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
