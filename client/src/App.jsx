8d49b2642e0334a77cc1a3066887c68d1a0f42f5
Chris K <Chris-SourceCode@outlook.com>
2023-01-09 05:36:36 +0100

    feat[client]: Temporary landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Register, Landing, Error, Home } from './pages';
+import { Register, Landing, Error } from './pages';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Landing />} />
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,31 @@
+import main from '../assets/images/main.svg';
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <nav>
+        <h2>Subslify</h2>
+      </nav>
+      <section className='container page'>
+        <div className='info'>
+          <h1>
+            Subscription <span>Tracking</span> App
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos ut
+            doloremque sapiente nam perferendis neque, distinctio dolores omnis
+            veniam blanditiis vel ducimus repellat aliquid unde error quasi
+            praesentium non tempore!
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img
+          src={main}
+          alt='subscription tracker'
+          className='img main-img'
+        ></img>
+      </section>
+    </div>
+  );
 };
 export default Landing;
