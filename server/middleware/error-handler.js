507a0f6167160ccdbcb56f999e43a54ee779db3b
Chris K <Chris-SourceCode@outlook.com>
2023-01-11 00:34:22 +0100

    feat[server]: Implement error handling
     - handle validation errors and duplicate fields

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,6 +1,24 @@
+import { StatusCodes } from 'http-status-codes';
+
 const errorHandlerMIddleware = (err, req, res, next) => {
   console.error(err.message);
-  res.status(500).json({ message: err.message });
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    message: 'Something went wrong',
+  };
+
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.message = err.message;
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.message = `Duplicate field value entered: ${Object.keys(
+      err.keyValue
+    )}`;
+  }
+
+  res.status(defaultError.statusCode).json({ message: defaultError.message });
 };
 
 export default errorHandlerMIddleware;
