9f6b472eb3d7eae54ef0796cc7604469ff03b4f9
Chris K <Chris-SourceCode@outlook.com>
2023-01-10 23:19:33 +0100

    feat[server]: Setup register function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,12 @@
+import User from '../models/User.js';
+
 const register = async (req, res) => {
-  res.send('Register');
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (err) {
+    res.status(400).json({ msg: err.message });
+  }
 };
 
 const login = async (req, res) => {
