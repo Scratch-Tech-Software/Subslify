8b465095a7abedcb126797efee84a22f6062a114
Chris K <Chris-SourceCode@outlook.com>
2023-01-12 22:26:46 +0100

    refactor[server]: Refactor custom errors

--- server/errors/bad-request.js
+++ server/errors/bad-request.js
@@ -3,8 +3,7 @@ import CustomAPIError from './custom-api.js';
 
 class BadRequestError extends CustomAPIError {
   constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
+    super(message, StatusCodes.BAD_REQUEST);
   }
 }
 
--- server/errors/custom-api.js
+++ server/errors/custom-api.js
@@ -1,9 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
 
 class CustomAPIError extends Error {
-  constructor(message) {
+  constructor(message, statusCode = StatusCodes.INTERNAL_SERVER_ERROR) {
     super(message);
-    this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+    this.statusCode = statusCode;
   }
 }
 
