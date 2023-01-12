41fa5cf2ebc4154228fb2b113846ac8840d1d127
Lily <hoonglily4@gmail.com>
2023-01-12 00:48:30 -0800

    finished navbar

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link, useResolvedPath, useMatch } from 'react-router-dom';
+import { NavLink, Link, useResolvedPath, useMatch } from 'react-router-dom';
 
 const NavBar = () => {
-  return (  
+  return (
     <nav className="navbar">
       <Link to="/dashboard" className='title'>Sublify</Link>
       <ul className="links">
-        <li><Link to='/dashboard'>Dashboard</Link></li>
-        <li><Link to='/register'>Register</Link></li>
-        <li><Link to='/landing'>Landing Page</Link></li>
-        <li><Link to='/active'>Active Subscriptions</Link></li>
+        <li><NavLink activeClassName="active" to='/dashboard'>Dashboard</NavLink></li>
+        <li><NavLink activeClassName="active" to='/register'>Register</NavLink></li>
+        <li><NavLink activeClassName="active" to='/landing'>Landing Page</NavLink></li>
+        <li><NavLink activeClassName="active" to='/active'>Active Subscriptions</NavLink></li>
       </ul>
-    </nav>
+    </nav>  
   );
 };
  
