f5ee268eda60a1f7c8b5bfc573fc078a7a0decae
Chris K <Chris-SourceCode@outlook.com>
2023-01-13 21:03:07 +0100

    feat[client]: Implement protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { Register, Landing, Error } from './pages';
+import { Register, Landing, Error, ProtectedRoute } from './pages';
 import { Active, Past, Summary, Trial, SharedLayout } from './pages/dashboard';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SharedLayout />}>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Active />} />
           <Route path='trial' element={<Trial />} />
           <Route path='past' element={<Past />} />
