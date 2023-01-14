492235f42408ed5a7cacec0da80fa642b3d37534
H3R01A <aallen0001@gmail.com>
2023-01-14 13:16:50 -0500

    updated demo version

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -12,13 +12,10 @@ const Landing = () => {
       <section className='container page'>
         <div className='info'>
           <h1>
-            Subscription <span>Tracking</span> App
+          Subscription overload?<span> Subslify</span> your life today!
           </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos ut
-            doloremque sapiente nam perferendis neque, distinctio dolores omnis
-            veniam blanditiis vel ducimus repellat aliquid unde error quasi
-            praesentium non tempore!
+          With <span>Subslify</span>, you can easily track, organize, and understand your monthly subscriptions. No more surprise charges or hidden fees - Subslify has you covered. Try it now and take control of your subscription-based expenses!
           </p>
           <Link to='/register' className='btn btn-hero'>
             Login/Register
