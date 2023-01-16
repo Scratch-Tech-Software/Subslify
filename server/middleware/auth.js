a325edbad636212d1f714ac7ae2f05682d89c3d5
Chris K <Chris-SourceCode@outlook.com>
2023-01-16 05:21:12 +0100

    feat[server]: Setup auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,19 @@
-const auth = async ({ headers: { authorization } }, res, next) => {
-  console.log({ authorization });
+import jwt from 'jsonwebtoken';
+import { UnauthenticatedError } from '../errors/index.js';
+
+const auth = async (req, _res, next) => {
+  const authHeaders = req.headers?.authorization;
+  if (!authHeaders || !authHeaders.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+
+  const token = authHeaders.substring(authHeaders.indexOf(' ') + 1);
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = { userId: payload.userId };
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
   next();
 };
 
