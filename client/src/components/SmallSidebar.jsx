286e39612f0137dbaf6d773b9202a7d50eb2aa75
Chris K <Chris-SourceCode@outlook.com>
2023-01-21 14:07:21 +0100

    feature[client]: Add mobile menu
     - Refactor navbar
    
     - Remove redundant styles
    
     - Change logo image reference

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,7 +1,25 @@
+import { useAppContext } from '../context/appContext';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
+
+import { FaTimes } from 'react-icons/fa';
 import '../assets/styles/small-sidebar.scss';
 
 const SmallSidebar = () => {
-  return <aside className='small-sidebar'>SmallSidebar</aside>;
+  const { showSidebar, toggleSidebar } = useAppContext();
+  return (
+    <aside className='small-sidebar'>
+      <div className={`sidebar-container ${showSidebar ? 'show-sidebar' : ''}`}>
+        <div className='content'>
+          <button className='close-btn' type='button' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <h3>Subslify</h3>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default SmallSidebar;
