54b962b2396353175f92ca700f2c29606a54bacf
Chris K <Chris-SourceCode@outlook.com>
2023-01-22 21:12:59 +0100

    fix[server]: Remove authenicate middleware from logout route

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -14,6 +14,6 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 router.route('/getCurrentUser').get(authenticateUser, getCurrentUser);
-router.route('/logout').get(authenticateUser, logout);
+router.route('/logout').get(logout);
 
 export default router;
