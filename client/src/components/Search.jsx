f3d504333a6e1cfcf9c6914c2c101760d65ca778
Tim Chang <tim.chang87@gmail.com>
2023-01-22 13:02:12 -0500

    Format code

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,17 +1,18 @@
 import '../assets/styles/search.scss';
 
 const Search = () => {
-    const handleChange = () => {};
-    return (
-        <div className="search">
-            <input
-                className="search-input"
-                type="search"
-                placeholder="Search..."
-                onChange={handleChange}
-            />
-        </div>
-    );
+  const handleChange = () => {};
+
+  return (
+    <div className='search'>
+      <input
+        className='search-input'
+        type='search'
+        placeholder='Search...'
+        onChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export default Search;
