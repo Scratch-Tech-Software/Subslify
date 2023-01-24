b24da586ebe2539fe303bb112efbdb749162a969
IuliiaSam <iuliia.samokhvalova@gmail.com>
2023-01-23 23:37:04 -0500

    subscriptionsController: implemented createSubscription, getSubscriptions

--- server/src/controllers/subscriptionsController.js
+++ server/src/controllers/subscriptionsController.js
@@ -1,5 +1,23 @@
+import Subscription from '../models/Subscription.ts';
+import { StatusCodes } from 'http-status-codes';
+import { UnAuthenticatedError } from '../errors/index.js';
+
 const createSubscription = async (req, res) => {
-  res.send('Create subscription');
+  const { name, price, frequency, startDate } = req.body;
+  const user = req.user.id;
+  if (!user) {
+    throw new UnAuthenticatedError('User not found');
+  }
+
+  await Subscription.create({
+    name,
+    price,
+    frequency,
+    startDate,
+    user,
+  });
+
+  res.status(StatusCodes.CREATED).json({ message: 'Subscription created' });
 };
 
 const updateSubscription = async (req, res) => {
@@ -10,13 +28,35 @@ const deleteSubscription = async (req, res) => {
   res.send('Delete subscription');
 };
 
-const getAllSubscriptions = async (req, res) => {
-  res.send('Get all subscriptions');
+const getSubscriptions = async (req, res) => {
+  const user = req.user.id;
+  const filter = {
+    user,
+    ...req.query, // TODO: sanitize query
+  };
+
+  const data = await Subscription.find(filter).exec();
+
+  const subscriptions = data.map((el) => ({
+    name: el.name,
+    price: el.price,
+    frequency: el.frequency,
+    status: el.status,
+    startDate: el.startDate,
+    notes: el.notes,
+  }));
+
+  res.status(StatusCodes.OK).json(subscriptions);
+};
+
+const getOneSubscription = async (req, res) => {
+  res.send('Get one subscription');
 };
 
 export {
   createSubscription,
   updateSubscription,
   deleteSubscription,
-  getAllSubscriptions,
+  getSubscriptions,
+  getOneSubscription,
 };
