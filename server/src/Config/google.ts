ac19330d84fa84a1ababee4cfaa2d2bcabf43781
zhiyi15 <zhiyihuang1994@gmail.com>
2023-01-25 17:23:22 -0800

    deleted addOauth in google.ts

--- server/src/Config/google.ts
+++ server/src/Config/google.ts
@@ -50,12 +50,6 @@ export default function (passport: any) {
           },
         };
 
-        const addOauth = {
-          provider: 'Google',
-          userId: profile.id,
-          userEmail: profile.emails[0].value,
-        };
-
         try {
           // check wether the email has been used for a user store in DB
           let user = await User.findOne({ email: profile.emails[0].value });
@@ -65,7 +59,7 @@ export default function (passport: any) {
             done(null, user);
             // else if check wether user.oauth is falsy(empty) then add then profile info to user.oauth
           } else if (!user.oauth) {
-            user.oauth = addOauth;
+            user.oauth = newUser.oauth;
             await user.save();
             done(null, user);
           }
