9ef6cda6b6a62dd6b01e72fd6662f77542494d9f
zhiyi15 <zhiyihuang1994@gmail.com>
2023-01-26 09:51:45 -0800

    added redirect route to frontend from server with authenticate JWT token

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import passport from 'passport';
+import attachCookies from '../utils/attachCookies.js';
 import {
   register,
   login,
@@ -15,28 +16,25 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 
-//GET /auth/google (successful request)
+//GET /auth/google 
 router.route('/google').get(
-  passport.authenticate('google'
-  // , (req, res, next) => {
-    // console.log('req.user', req.user);
-    // console.log('req.session.passport.user:', req.session.passport.user);
-  // }
+  passport.authenticate(
+    'google'
   )
 );
 
 router.route('/google/redirect').get(
-  passport.authenticate(
-    'google',
-    {
-      // successRedirect: process.env.AUTH_SUCCESS_ROUTE,
-      failureRedirect: process.env.AUTH_FAILURE_ROUTE
-    }
-),
-/* use authController to register user, and access / endpoint with a token*/ 
-(req, res) => {
-  res.redirect(process.env.AUTH_SUCCESS_ROUTE)
-});
+  passport.authenticate('google', {
+    failureRedirect: process.env.AUTH_FAILURE_ROUTE
+  }),
+  /* create JWT token, with the authenticate token to access '/' endpoint */
+  (req, res) => {
+    console.log('req/user:', req.user); //user info stored in req.user from passport deserialize
+    const token = req.user.createJWT();
+    attachCookies({ res, token });
+    res.redirect(process.env.AUTH_SUCCESS_ROUTE);
+  }
+);
 
 router.route('/getCurrentUser').get(authenticateUser, getCurrentUser);
 router.route('/logout').get(logout);
